eceab7dc5b52f6724b1211f9ecd88c48f71bc125
Railos <sirius.ilya.info01@gmail.com>
2025-04-23 18:39:43 +0300

    login

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from data import db_session
 from data.users import User
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
+import logging
+from flask_login import LoginManager, login_required, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'markdaun'
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
@@ -38,10 +41,31 @@ def register():
         return redirect('/login')
     return render_template('register.html', menuname="Регистрация", form=form)
 
-@app.route('/login')
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', menuname="Вход")
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form, menuname="Вход")
+    return render_template('login.html', menuname="Вход", form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
